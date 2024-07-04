d75b3148c61fba160b2eb77203551336fd93aae6
Victor Moreno <victor.moreno@cabify.com>
2024-07-04 18:26:27 +0200

    refactor: refine test name

--- test/bank/services_test.exs
+++ test/bank/services_test.exs
@@ -8,7 +8,7 @@ defmodule Test.Bank.Services do
   alias Test.Support.MoneyMother
 
   describe "open an account" do
-    test "should open an account in a specific currency" do
+    test "should open an account in a specific currency and get its id" do
       account_id = Services.open_account(:USD)
       assert AccountId.is(account_id)
     end
