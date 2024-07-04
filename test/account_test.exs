1a378949e6b3145fdc6445d742a2abc4658fe048
Victor Moreno <victor.moreno@cabify.com>
2024-07-04 16:00:07 +0200

    refactor: models dir

--- test/account_test.exs
+++ test/account_test.exs
@@ -1,61 +1,67 @@
-defmodule AccountTest do
+defmodule Bank.AccountTest do
   use ExUnit.Case
   doctest Bank.Account
 
   alias Bank.Account
   alias Test.Support.MoneyMother
 
-  test "should have 0 balance on open" do
-    assert Account.balance_of(Account.open()) == 0
+  describe "open an account" do
+    test "should have 0 balance on open" do
+      assert Account.balance_of(Account.open()) == 0
+    end
+
+    test "should have a unique id" do
+      account1 = Account.open()
+      account2 = Account.open()
+      refute account1 == account2
+    end
+
+    test "should allow opening an account in a specific currency" do
+      account = Account.open(:USD)
+
+      assert Account.currency_of(account) == :USD
+    end
   end
 
-  test "should have a unique id" do
-    account1 = Account.open()
-    account2 = Account.open()
-    refute account1 == account2
+  describe "deposit" do
+    test "should add balance on deposit" do
+      account =
+        Account.open()
+        |> Account.deposit(MoneyMother.eur(100))
+
+      assert Account.balance_of(account) == 100
+    end
+
+    test "should prevent deposits with different currencies" do
+      account = Account.open(:EUR)
+
+      assert {:error, :currency_mismatch} =
+               Account.deposit(account, MoneyMother.usd(100))
+    end
   end
 
-  test "should add balance on deposit" do
-    account =
-      Account.open()
-      |> Account.deposit(MoneyMother.eur(100))
+  describe "withdrawals" do
+    test "should withdraw balance" do
+      account =
+        Account.open()
+        |> Account.deposit(MoneyMother.eur(100))
+        |> Account.withdraw(MoneyMother.eur(50))
 
-    assert Account.balance_of(account) == 100
-  end
+      assert Account.balance_of(account) == 50
+    end
 
-  test "should allow opening an account in a specific currency" do
-    account = Account.open(:USD)
+    test "should prevent withdrawals with different currencies" do
+      account = Account.open(:EUR)
 
-    assert Account.currency_of(account) == :USD
-  end
+      assert {:error, :currency_mismatch} =
+               Account.withdraw(account, MoneyMother.usd(100))
+    end
 
-  test "should prevent deposits with different currencies" do
-    account = Account.open(:EUR)
+    test "should prevent withdrawals with insufficient funds" do
+      account = Account.open()
 
-    assert {:error, :currency_mismatch} =
-             Account.deposit(account, MoneyMother.usd(100))
-  end
-
-  test "should withdraw balance" do
-    account =
-      Account.open()
-      |> Account.deposit(MoneyMother.eur(100))
-      |> Account.withdraw(MoneyMother.eur(50))
-
-    assert Account.balance_of(account) == 50
-  end
-
-  test "should prevent withdrawals with different currencies" do
-    account = Account.open(:EUR)
-
-    assert {:error, :currency_mismatch} =
-             Account.withdraw(account, MoneyMother.usd(100))
-  end
-
-  test "should prevent withdrawals with insufficient funds" do
-    account = Account.open()
-
-    assert {:error, :insufficient_funds} =
-             Account.withdraw(account, MoneyMother.eur(100))
+      assert {:error, :insufficient_funds} =
+               Account.withdraw(account, MoneyMother.eur(100))
+    end
   end
 end
